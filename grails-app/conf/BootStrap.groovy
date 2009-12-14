2f587508385721c240cd60d4dd5a53323e0c4cdd
Gabriel Gayan Fanta <gabrielgayan@gmail.com>
2009-12-14 13:17:23 -0300

    add test user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,27 @@
+import cl.peloteros.User
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+    def init = { servletContext ->
+        environments{
+            development{
+                //agrego un usuario de prueba
+                def testUser = new User(
+                    name:"testUser",
+                    lastname:"testUser",
+                    login:"test@test.com",
+                    password:"1234",
+                )
+                testUser.save()
+            }
+        }
+    }
+    def destroy = {
+        environments{
+            development{
+                //elimino el usuario de prueba
+                User.findByName("testUser").delete()
+            }
+        }
+    }
 } 
